193d2d2921c07c429692116635295ffd8080b33b
Itati2003 <aguilera.o.itati@gmail.com>
2024-10-21 09:37:51 -0600

    Update procesar_login.php

--- procesar_login.php
+++ procesar_login.php
@@ -5,9 +5,19 @@ if($conn){
      echo "<br>";
   error_reporting(E_ALL);
     ini_set('display_errors' ,1);
-if(isset($_POST['registrar'])){
-
-}
-
+    if(isset($_POST['session'])){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $sql="SELECT * FROM usuarios WHERE email = '$email' AND password = '$password'";
+        $result = $conn->query($sql);
+        if($result && $result->num_rows > 0);{
+            $user=$result->fetch_assoc();
+            $_SESSION['nombre']=$user['nombre'];
+            header("Location: productos.html");
+        }else{
+        echo "Correo o contraseña incorrectos";
+        }
+    }
+    $conn->close();
 }
 ?>
