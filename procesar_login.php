149e6e2082750250f3ca5cd7b85bdd45af1ce808
Itati2003 <aguilera.o.itati@gmail.com>
2024-10-21 09:25:44 -0600

    Update procesar_login.php

--- procesar_login.php
+++ procesar_login.php
@@ -3,7 +3,11 @@ include ("conexion.php");
 if($conn){
     echo "Conexion exitosa";
      echo "<br>";
+  error_reporting(E_ALL);
+    ini_set('display_errors' ,1);
+if(isset($_POST['registrar'])){
 
+}
 
 }
 ?>
