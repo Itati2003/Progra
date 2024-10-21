573464d7f576e745159725f1661f3839aa2b0ba3
Itati2003 <aguilera.o.itati@gmail.com>
2024-10-21 12:40:25 -0600

    Update carrito.php

--- carrito.php
+++ carrito.php
@@ -33,27 +33,41 @@
 
     <section>
         <h1>Carrito de Compras</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Producto</th>
-                    <th>Cantidad</th>
-                    <th>Precio</th>
-                    <th>Total</th>
-                    <th>Acción</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Producto 1</td>
-                    <td>2</td>
-                    <td>20$</td>
-                    <td>40$</td>
-                    <td><button>Eliminar</button></td>
-                </tr>
-                <!-- Repetir para otros productos en el carrito -->
-            </tbody>
-        </table>
+        <div class="producto">
+            <ul>
+                <?
+                    if(isset($_SESSION['carrito'])){
+                        $total=0;
+                        for($i=0;$i<=count($carrito_user)-1;$i ++){
+                            if($carrito_user[$i]!=NULL){
+                            ?>
+                            <li>
+                                <div class="card-body">
+                                     <h5 class="card-title">Cantidad: <?php echo $carrito_user[$i]['cantidad']?> : <? echo $carrito_user[$i]['producto']; ?></h5>
+                                    <span class="text-muted"><? echo $carrito_user[$i]['precio'] * $carrito_user[$i]['cantidad']; ?></span>
+                                </div>
+                            </li>
+                            <?
+                            $total=$total + ($carrito_user[$i]['precio'] * $carrito_user[$i]['cantidad']);
+                        }
+                    }
+                    }
+                    ?>
+                    <li>
+                        <span> Total: (EUR)</span>
+                        <strong><?php
+                        if(isset($_SESSION['carrito'])){
+                            $total=0;
+                            for($i=0;$i<=count($carrito_user)-1;$i ++){
+                                if($carrito_user[$i]!=NULL){
+                                    $total=$total +($carrito_user[$i]['precio'] * $carrito_user[$i]['cantidad']);
+                                }
+                            }
+                        }
+                        echo $total; ?> $</strong>
+                    </li>
+            </ul>
+        </div>
         <button>Realizar pago</button>
     </section>
 
