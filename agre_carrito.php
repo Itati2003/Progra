ce04e95339d8d9c2e599979914c71dbb3e1d5f41
Itati2003 <aguilera.o.itati@gmail.com>
2024-10-21 12:46:28 -0600

    Update agre_carrito.php

--- agre_carrito.php
+++ agre_carrito.php
@@ -1,25 +1,23 @@
-der<?php session_start();
+<?php session_start();
 if (!isset($_SESSION['carrito'])) {
     $_SESSION['carrito'] = array();
 }
 $carrito_user = $_SESSION['carrito'];
 
-  if(isset($_SESSION['carrito'])){
-    $carrito_user=$_SESSION['carrito'];
-    if(isset($_POST['producto'])){
-      $producto=$_POST['producto'];
-      $precio=$_POST['precio'];
-      $cantidad=$_POST['cantidad'];
-      $num=0;
-      $carrito_user[]=array("Producto"=>$producto. "precio"=>$precio, "cantidad"=>$cantidad;
+if (isset($_SESSION['carrito'])) {
+    $carrito_user = $_SESSION['carrito'];
+    if (isset($_POST['producto'])) {
+        $producto = $_POST['producto'];
+        $precio = $_POST['precio'];
+        $cantidad = $_POST['cantidad'];
+        $carrito_user[] = array("Producto" => $producto, "precio" => $precio, "cantidad" => $cantidad);
     }
-  }else{
-     $producto=$_POST['producto'];
-      $precio=$_POST['precio'];
-      $cantidad=$_POST['cantidad'];
-      $num=0;
-      $carrito_user[]=array("Producto"=>$producto. "precio"=>$precio, "cantidad"=>$cantidad;
-  }
-$_SESSION['carrito']=$carrito_user;
-header("Location: ".$_SERVER['HTTP_REFERER']."");
+} else {
+    $producto = $_POST['producto'];
+    $precio = $_POST['precio'];
+    $cantidad = $_POST['cantidad'];
+    $carrito_user[] = array("Producto" => $producto, "precio" => $precio, "cantidad" => $cantidad);
+}
+$_SESSION['carrito'] = $carrito_user;
+header("Location: " . $_SERVER['HTTP_REFERER']);
 ?>
