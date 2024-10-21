b994bd0f4936238c9b579345298f5e1df56942ff
Itati2003 <aguilera.o.itati@gmail.com>
2024-10-21 11:28:31 -0600

    Update and rename productos.html to productos.php

---
+++ productos.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catálogo de Productos</title>
+    <link rel="stylesheet" href="diseno.css">
+</head>
+<body>
+    <header>
+        <h1>Mi Tienda</h1>
+        <nav>
+            <ul>
+                <li><a href="pagina_de_inicio.html">Inicio</a></li>
+                <li><a href="productos.html">Productos</a></li>
+                <li><a href="carrito.html">Carrito</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section>
+        <h1>Catálogo de Productos</h1>
+        <div class="producto">
+            <form id="formulario" name="formulario" method="post" action="carrito.php">
+            <input name="precio" id="precio" value="50" />
+            <input name="producto" id="producto" value="Producto1" />
+            <img src="producto1.jpg" alt="Producto 1">
+                <div class="card-body">
+                    <h5 class="card-title"> Producto 1</h5>
+                    <p class="card-text">Precio $50</p>
+                    <button type="submit">Añadir al Carrito</button>
+                </div>
+            </form>
+        </div>
+        <div class="producto">
+            <<form id="formulario" name="formulario" method="post" action="carrito.php">
+            <input name="precio" id="precio" value="900" />
+            <input name="producto" id="producto" value="Producto2" />
+            <img src="producto2.jpg" alt="Producto 2">
+                <div class="card-body">
+                    <h5 class="card-title"> Producto 2</h5>
+                    <p class="card-text">Precio $900</p>
+                    <button type="submit">Añadir al Carrito</button>
+                </div>
+            </form>
+        </div>
+        <div class="producto">
+            <<form id="formulario" name="formulario" method="post" action="carrito.php">
+            <input name="precio" id="precio" value="900" />
+            <input name="producto" id="producto" value="Producto2" />
+            <img src="producto2.jpg" alt="Producto 2">
+                <div class="card-body">
+                    <h5 class="card-title"> Producto 2</h5>
+                    <p class="card-text">Precio $900</p>
+                    <button type="submit">Añadir al Carrito</button>
+                </div>
+            </form>
+        </div>
+    </section>
+
+    <footer>
+        <p>© 2024 Mi Tienda. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
