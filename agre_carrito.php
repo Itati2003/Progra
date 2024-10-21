9f651b400f081890056855edff13ff8d45302b96
Itati2003 <aguilera.o.itati@gmail.com>
2024-10-21 13:12:45 -0600

    Update agre_carrito.php

--- agre_carrito.php
+++ agre_carrito.php
@@ -4,20 +4,27 @@ if (!isset($_SESSION['carrito'])) {
 }
 $carrito_user = $_SESSION['carrito'];
 
-if (isset($_SESSION['carrito'])) {
-    $carrito_user = $_SESSION['carrito'];
-    if (isset($_POST['producto'])) {
-        $producto = $_POST['producto'];
-        $precio = $_POST['precio'];
-        $cantidad = $_POST['cantidad'];
-        $carrito_user[] = array("Producto" => $producto, "precio" => $precio, "cantidad" => $cantidad);
-    }
-} else {
+if (isset($_POST['producto'])) {
     $producto = $_POST['producto'];
     $precio = $_POST['precio'];
     $cantidad = $_POST['cantidad'];
-    $carrito_user[] = array("Producto" => $producto, "precio" => $precio, "cantidad" => $cantidad);
+    $encontrado = false;
+
+    // Verificar si el producto ya está en el carrito
+    for ($i = 0; $i < count($carrito_user); $i++) {
+        if ($carrito_user[$i]['Producto'] == $producto) {
+            $carrito_user[$i]['cantidad'] += $cantidad;
+            $encontrado = true;
+            break;
+        }
+    }
+
+    // Si el producto no está en el carrito, agregarlo
+    if (!$encontrado) {
+        $carrito_user[] = array("Producto" => $producto, "precio" => $precio, "cantidad" => $cantidad);
+    }
 }
+
 $_SESSION['carrito'] = $carrito_user;
 header("Location: " . $_SERVER['HTTP_REFERER']);
 ?>
