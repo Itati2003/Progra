80c7e5421f961902395ad8e7661ba6cf69915d12
Itati2003 <aguilera.o.itati@gmail.com>
2024-10-21 00:45:20 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,13 +3,6 @@ $servername = "localhost";
 $username = "atenea";
 $password = "123";
 $database = "baseDAt";
-
-//Crear conexion
-
 $conn = new mysqli($servername, $username, $password, $database);
 
-if ($conn->connect_error) {
-    die("conexion fallida: ". $conn->connect_error);
-}
-echo "Conexion exitosa";
 ?>
