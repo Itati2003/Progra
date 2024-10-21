47f9e2761aeabdbc5a9ea0a3820e779968ba0eaa
Itati2003 <aguilera.o.itati@gmail.com>
2024-10-21 01:04:41 -0600

    Update procesar_usuario.php

--- procesar_usuario.php
+++ procesar_usuario.php
@@ -4,18 +4,19 @@ if($conn){
     echo "Conexion exitosa";
 #error_reporting(E_ALL);
 #ini_set('display_errors' ,1);
-if(isse($_POST['registrar'])){
-     echo "Post";
- #   $nombre=$_POST['nombre'];
-  #  $email=$_POST['email'];
- #   $password=$_POST['password'];
-  #  $sql="INSERT INTO usuarios (nombre, email, password) VALUES ('$nombre','$email','$password');
+      $nombre=$_POST['nombre'];
+      $email=$_POST['email'];
+      $password=$_POST['password'];
+     $sql="INSERT INTO usuarios (nombre, email, password) VALUES ('$nombre','$email','$password');
+     if($sql){
+      echo "exitosa";
+      }
     #if($conn->quey($sql)===TRUE){
   #      echo "Registro exitoso";
   #  } else {
   #      echo "Error: ". $sql ."<br>". $conn->error; 
  #   }
  #   $conn->close();
-}
+
 }
 ?>
