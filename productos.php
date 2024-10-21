0f201a3c241ae1a8626d07450fe7ed7cde73daee
Itati2003 <aguilera.o.itati@gmail.com>
2024-10-21 11:45:06 -0600

    Update productos.php

--- productos.php
+++ productos.php
@@ -25,7 +25,7 @@
             <form id="formulario" name="formulario" method="post" action="carrito.php">
             <input name="precio" type="hidden" id="precio" value="50" />
             <input name="producto" type="hidden" id="producto" value="Producto1" />
-            <img src="Producto1.jpeg" alt="Producto 1">
+            <img src="Producto1.jpeg" alt="Producto1">
                 <div class="card-body">
                     <h5 class="card-title"> Producto 1</h5>
                     <p class="card-text">Precio $50</p>
@@ -37,7 +37,7 @@
             <<form id="formulario" name="formulario" method="post" action="carrito.php">
             <input name="precio" type="hidden" id="precio" value="900" />
             <input name="producto" type="hidden" id="producto" value="Producto2" />
-            <img src="Producto2.jpeg" alt="Producto 2">
+            <img src="Producto2.jpeg" alt="Producto2">
                 <div class="card-body">
                     <h5 class="card-title"> Producto 2</h5>
                     <p class="card-text">Precio $900</p>
@@ -47,12 +47,12 @@
         </div>
         <div class="producto">
             <<form id="formulario" name="formulario" method="post" action="carrito.php">
-            <input name="precio" type="hidden" id="precio" value="900" />
+            <input name="precio" type="hidden" id="precio" value="1230" />
             <input name="producto" type="hidden" id="producto" value="Producto2" />
-            <img src="Producto3.jpeg" alt="Producto 2">
+            <img src="Producto3.jpeg" alt="Producto3">
                 <div class="card-body">
                     <h5 class="card-title"> Producto 2</h5>
-                    <p class="card-text">Precio $900</p>
+                    <p class="card-text">Precio $1230</p>
                     <button type="submit">Añadir al Carrito</button>
                 </div>
             </form>
